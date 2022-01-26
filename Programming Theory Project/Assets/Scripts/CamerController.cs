483c07acd276ca700ef97fdfe9e25b03d6311c36
AhmedSaadTanim <astdragneel@yahoo.com>
2022-01-27 03:06:29 +0600

    blocks added

--- Programming Theory Project/Assets/Scripts/CamerController.cs
+++ Programming Theory Project/Assets/Scripts/CamerController.cs
@@ -2,21 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CamerController : MonoBehaviour {
+public class CamerController : MonoBehaviour
+{
     public float speed;
     public float clampLeft;
     public float clampRight;
-
+    public Animator cameraAnimation;
+    private int counter = 0, animationDuration = 15;
     private float cameraX;
-
+    private bool gameStart;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         cameraX = transform.position.x;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        cameraAnimation = GameObject.Find("Main Camera").GetComponent<Animator>();
+        StartCoroutine(AnimationPlay());
+    }
+
+    IEnumerator AnimationPlay()
+    {
+        while (counter != animationDuration)
+        {
+            yield return new WaitForSeconds(1f);
+            counter++;
+        }
+        stopAnimation();
+    }
+
+    void stopAnimation()
+    {
+        cameraAnimation.enabled = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
         if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < clampRight)
         {
             transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
@@ -25,9 +44,7 @@ public class CamerController : MonoBehaviour {
         {
             transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
         }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Debug.Log(cameraX);
-        }
+
     }
+
 }
